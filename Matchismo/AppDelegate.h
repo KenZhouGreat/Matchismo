165c8af0007ebd1c22e2563fe5170dce5775d02c
Ken Zhou <KenZhouGreat@users.noreply.github.com>
2014-01-02 22:24:33 +1100

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Ken Zhou on 2/01/2014.
+//  Copyright (c) 2014 Ken Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Ken Zhou on 2/01/2014.
+//  Copyright (c) 2014 Ken Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
